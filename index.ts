bc7bc3df91b1864ac4b77e08bdcc050eb0d17257
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2023-01-14 17:17:34 +0000

    Add console interface

--- index.ts
+++ index.ts
@@ -1,20 +1,78 @@
-/*import { clear, print, xxx } from './console'; */
+import { clear, print, askQuestion } from './console';
+import { readFileSync } from 'fs';
+import { RoverPosition } from './src/moveRover.js';
 import { move } from './src/moveRover.js';
 
-/*export function initialiseRovers(): void {
+
+export function initialiseRover(): void {
 	clear(false);
-	print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
-	print('User <McUser> cleared for security');
-	print('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'); */
+	print('...................................................................');
+	print('User credentials recognised. Please wait for Rover incoming signals.');
+	print('....................................................................');
 
-	//selecttRover(`Please select an active Rover` ');
-	//selecttRover(`Please enter Rover navigation instructions using characters L, M and/or R. Instructions must be no more than 15 characters.` ');
-
-	//Temporary parameters:
+	const RoverInventory = readFileSync('./Rover_log/inventory.txt');
+	print (RoverInventory.toString());
 
 
-let _RoverID = '';
-let _startingPoint = '';
-let _instructions = '';
 
-move(_RoverID, _startingPoint, _instructions);
+   askQuestion(`Enter the ID of the active Rover that you want to navigate. `, selectRover);
+
+	let lastPosition: RoverPosition = {
+		Roverid: '',
+		xPosition: 0,
+		yPosition: 0,
+		orientation: ''
+	}	
+
+	let instructions = '';
+
+    //Use the Rover's id to find its most recent coordinates from the log	
+   function selectRover (ID: string): void {
+
+     const data = readFileSync('./Rover_log/position_log.txt');
+     const RoverData = data.toString().split('\n');	
+	 const lastCoordinates = RoverData.find (coordinates => coordinates.match(ID));
+
+	 if (lastCoordinates === undefined) {'error - contact mission control'} //end if
+
+	 else {
+
+	//Prepare the Rover to move
+	  lastPosition.Roverid = lastCoordinates[0],
+	  lastPosition.xPosition = Number(lastCoordinates[1][0]),
+      lastPosition.yPosition =  Number(lastCoordinates[1][1]),
+      lastPosition.orientation = lastCoordinates[1][2]
+
+	 }	 //end else
+
+	 askQuestion(`Enter navigation instructions of no more than 15 characters (M, L or R) Rover`, inputInstructions);
+
+     function  inputInstructions (userInstructions: string) {
+       
+		if ((userInstructions.length > 15) || (userInstructions === undefined)) 
+		  { print('Invalid instructions')} //end print instructions
+
+		else {
+			instructions = userInstructions;
+		} //end else instructions
+     }	//end input function
+  }	//end of selection
+
+  console.log (instructions, lastPosition );
+ 
+} //end of initialise
+
+initialiseRover ();
+
+
+
+/*More sophisticated functionality is needed to handle for multiple log entries with different dates
+	
+	//const loggedCoordinates = Array.from(RoverData).filter (coordinates => coordinates.match(matchWithID));
+	//lastPowerDownDate (loggedCoordinates);
+
+    /*function lastPowerDownDate (loggedCoordinates) {
+     const logDates = loggedCoordinates.map (coordinates => Number(coordinates.split(' ')[2]));
+   }
+ */  
+
